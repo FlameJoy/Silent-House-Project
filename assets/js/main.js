e1ca83e8aeeb40d13b68e09d3792a1422bdfd537
FlameJoy <ilia.val07@gmail.com>
2022-01-20 21:43:24 +0300

    Fix multislider

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,31 +3,34 @@
 // --------------
 // Slider
 // --------------
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1, 1];
+let slideID = ["slide1", "slide2"];
+let dotsID = ["slider-dot1", "slider-dot2"];
+showSlides(1, 0);
+showSlides(1, 1);
 
 // Next/previous controls
-function plusSlide(n) {
-  showSlides(slideIndex += n);
+function plusSlide(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
 }
 
-function showSlides(n) {
+function showSlides(n, no) {
   let i;
-  let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("slider-dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let slides = document.getElementsByClassName(slideID[no]);
+  let dots = document.getElementsByClassName(dotsID[no]);
+  if (n > slides.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+  slides[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[no]-1].className += " active";
+}
