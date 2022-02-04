f8e1d0c71dc4cdcbb935afe5747b2183ef1e0940
FlameJoy <ilia.val07@gmail.com>
2022-02-04 17:51:13 +0300

    design renovation block

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ let dotsID = ["slider-dot1", "slider-dot2", "slider-dot3", "slider-dot4"];
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
-// showSlides(1, 3);
+showSlides(1, 3);
 
 // Next/previous controls
 function plusSlide(n, no) {
